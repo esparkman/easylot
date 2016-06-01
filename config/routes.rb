b7ebe4ac00fb9eeed7b5ca86fcf76100115c8faa
Evan Sparkman <evan.sparkman@manheim.com>
2016-05-31 22:15:24 -0400

    Add routes for announcements

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :photos, only: [:create, :destroy]
   end
 
+  resources :announcements
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
