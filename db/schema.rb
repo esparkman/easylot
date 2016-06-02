5bb918f2d7062019d3bf4033eb2394d8afbeb407
Evan Sparkman <me@evansparkman.com>
2016-06-02 01:51:14 -0400

    Fixup: Typo on Vehicle Form

--- db/schema.rb
+++ db/schema.rb
@@ -23,22 +23,28 @@ ActiveRecord::Schema.define(version: 20160601015059) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.boolean  "admin",                  default: false
+  create_table "authem_sessions", force: :cascade do |t|
+    t.string   "role",                    null: false
+    t.integer  "subject_id",              null: false
+    t.string   "subject_type",            null: false
+    t.string   "token",        limit: 60, null: false
+    t.datetime "expires_at",              null: false
+    t.integer  "ttl",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "authem_sessions", ["expires_at", "subject_type", "subject_id"], name: "index_authem_sessions_subject", using: :btree
+  add_index "authem_sessions", ["expires_at", "token"], name: "index_authem_sessions_on_expires_at_and_token", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                                           null: false
+    t.string   "password_digest",                                 null: false
+    t.string   "password_reset_token", limit: 60,                 null: false
+    t.boolean  "admin",                           default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.string   "make"
