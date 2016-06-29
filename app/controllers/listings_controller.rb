3679eaf2afd557b29eef46445636c98d2866e000
Evan Sparkman <evan.sparkman@manheim.com>
2016-06-29 08:36:47 -0400

    Fixup: Scope Listings page to featured vehicles only

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  expose(:vehicles) { Vehicle.all }
+  expose(:vehicles) { Vehicle.where(featured: true) }
   expose(:announcements) { Announcement.all }
   expose(:results) { Vehicle.search(params.fetch(:query, "*")) }
 end
