c0823b7f14cd1a227a7f308daeb5f14c9a797885
Evan Sparkman <evan.sparkman@manheim.com>
2016-07-08 00:08:33 -0400

    Make Vehicle indexable by Bonsai

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,7 @@
 class Vehicle < ActiveRecord::Base
   mount_uploaders :photos, PhotoUploader
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   searchkick word_start: [:make, :model, :year]
   # searchkick text_start: [:make, :model, :year]
